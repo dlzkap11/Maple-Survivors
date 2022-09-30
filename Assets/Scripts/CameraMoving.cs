7b828afdbcfe420f218ba3c5993c30c478761c94
dlzkap11 <dlzkap11@naver.com>
2022-10-01 00:21:15 +0900

    config: Update default data.
    - first commit

---
+++ Assets/Scripts/CameraMoving.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoving : MonoBehaviour
+{
+    public GameObject target;
+    Transform tr;
+    
+   
+
+    void Start()
+    {
+        tr = target.transform;
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(tr.position.x , tr.position.y, -10);
+
+    }
+}
