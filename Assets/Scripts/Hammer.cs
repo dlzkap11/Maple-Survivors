1e572a19bbd0991acf48e72ebaedd44040766336
KINjoohyun <gurados@gmail.com>
2023-01-05 23:53:46 +0900

    Hammer 회전 기능 추가

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Hammer : MonoBehaviour
 {
     public int weaponLevel; //레벨
-    public float dmg;
+
+    public float dmg; //데미지
 
     private float spinSpeed; //회전속도
     
     void Start()
     {
         dmg = 10 * weaponLevel;
+        spinSpeed = weaponLevel * 0.5f;
     }
 
     void Update()
     {
-        
+        transform.RotateAround(Vector3.zero, Vector3.back, spinSpeed);
     }
+    
 }
